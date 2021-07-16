f62f5824db31a0e34640acc4c8ca9c6c897df2c2
RitikaHooda <65641465+RitikaHooda@users.noreply.github.com>
2021-07-16 15:54:37 +0530

    Update emotion_analysis.py

--- emotion_analysis.py
+++ emotion_analysis.py
@@ -3,6 +3,9 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import datetime
+import csv
+import os
 
 
 class FaceDetection(object):
@@ -76,3 +79,63 @@ class EmotionPrediction(object):
 
     def getEmotionsLabels(self):
         return self.labels
+    
+    
+def saveCsvFile(video_file, output_file='output/out.csv', total_faces=0, emotions_dict={}, emotions=[]):
+    data = []
+    if not os.path.isfile(output_file):
+        data = ['Video path', 'Total of faces', 'Neutral Emotions', 'Happy Emotions', 'Sad Emotions',
+                'Surprise Emotions', 'Angry Emotions']
+    else:
+        data.append(video_file)
+        data.append(total_faces)
+        for i in emotions:
+            data.append(emotions_dict[i])
+
+    with open(output_file, mode='a') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(data)
+
+def main():
+    face_detection = FaceDetection()
+    emotion_prediction = EmotionPrediction()
+    # Capturing Video from primary webcam, you can change number from 0 to any Integer
+    ## for other webcams if you have many of them
+    cap = cv.VideoCapture(0)
+    emotions = emotion_prediction.getEmotionsLabels()
+    start_time = datetime.datetime.now()
+    output_file = "output/out.csv"
+    os.remove(output_file)
+    video_file = "camera"  ## TODO choose according to the no of faces detected
+    while cap.isOpened():
+        # Reading frame from the precorded video.
+        ret, frame = cap.read()
+        if np.shape(frame) == ():
+            break
+
+        face, img = face_detection.predict(frame)
+
+        if np.shape(img) == ():
+            print("img is empty")
+        else:
+            cv.imshow('img', img)
+            cv.waitKey(10)
+
+        if face is not None:
+            emotion_prediction.predict(face)
+
+        check_time = datetime.datetime.now()
+
+        if (check_time - start_time).total_seconds() > 3:
+            emotions_dict = emotion_prediction.getEmotions()
+            total_faces = face_detection.totalDetections()
+            saveCsvFile(video_file, output_file=output_file, total_faces=total_faces, emotions_dict=emotions_dict,
+                        emotions=emotions)
+            start_time = datetime.datetime.now()
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
